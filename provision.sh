60115df6da8bb32c60e934b3c7e022f13354a012
jacky <jacksonalmeidada@gmail.com>
2019-11-08 12:52:21 +0100

    segundo commit

--- provision.sh
+++ provision.sh
@@ -1,5 +1,11 @@
 apt-get -y update
+
 apt-get install -y apache2
+
+apt-get install -y php7.0 php7.0-cli php7.0-common php7.0-mbstring php7.0-intl php7.0-xml php7.0-mysql php7.0-mcrypt
+
+apt-get install mysql-server
+
 
 sudo mkdir /almeidaweb
 sudo mkdir /almeidaweb/html/uno.html
